10e80f81f10e71100db7da5666c37d8d4c128a5c
Tomek <Tomek@DESKTOP-SIK5VAJ>
2022-11-26 18:37:08 +0100

    Add project files.

---
+++ Lines/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Lines
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.increment = new System.Windows.Forms.TextBox();
+            this.lenght = new System.Windows.Forms.TextBox();
+            this.angle = new System.Windows.Forms.TextBox();
+            this.lines = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.increment);
+            this.panel2.Controls.Add(this.lenght);
+            this.panel2.Controls.Add(this.angle);
+            this.panel2.Controls.Add(this.lines);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1004, 58);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(746, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "GO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // increment
+            // 
+            this.increment.Location = new System.Drawing.Point(626, 12);
+            this.increment.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.increment.Name = "increment";
+            this.increment.Size = new System.Drawing.Size(110, 23);
+            this.increment.TabIndex = 7;
+            this.increment.Text = "1";
+            // 
+            // lenght
+            // 
+            this.lenght.Location = new System.Drawing.Point(409, 12);
+            this.lenght.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lenght.Name = "lenght";
+            this.lenght.Size = new System.Drawing.Size(110, 23);
+            this.lenght.TabIndex = 6;
+            this.lenght.Text = "5";
+            // 
+            // angle
+            // 
+            this.angle.Location = new System.Drawing.Point(228, 12);
+            this.angle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.angle.Name = "angle";
+            this.angle.Size = new System.Drawing.Size(88, 23);
+            this.angle.TabIndex = 5;
+            this.angle.Text = "74";
+            // 
+            // lines
+            // 
+            this.lines.Location = new System.Drawing.Point(84, 12);
+            this.lines.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lines.Name = "lines";
+            this.lines.Size = new System.Drawing.Size(80, 23);
+            this.lines.TabIndex = 4;
+            this.lines.Text = "100";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(533, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Increment";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(340, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lenght";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(170, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Angle";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(25, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "#lines";
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 58);
+            this.canvas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1004, 430);
+            this.canvas.TabIndex = 2;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 488);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fun With Line Patterns";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox increment;
+        private System.Windows.Forms.TextBox lenght;
+        private System.Windows.Forms.TextBox angle;
+        private System.Windows.Forms.TextBox lines;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel canvas;
+    }
+}
+
---
+++ Lines/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lines
+{
+    public partial class Form1 : Form
+    {
+        Pen myPen = new Pen(Color.Black);
+        Pen myPenBlue = new Pen(Color.Blue);
+        Pen myPenRed = new Pen(Color.Red);
+        Graphics g = null;
+        
+        static int center_x, center_y;
+        static int start_x, start_y;
+        static int end_x, end_y;
+
+        static int my_angle = 0;
+        static int my_lenght = 0;
+        static int my_increment = 0;
+        static int num_lines = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            my_lenght = Int32.Parse(lenght.Text);
+            my_increment = Int32.Parse(increment.Text);
+            my_angle = Int32.Parse(angle.Text);
+            num_lines = Int32.Parse(lines.Text);
+
+            start_x = canvas.Width / 2;
+            start_y = canvas.Height / 2;
+
+            canvas.Refresh();
+        }
+
+      
+
+        private void drawLine()
+        {
+            Point p1 = new Point((canvas.Width / 2), 0);
+            Point p2 = new Point(canvas.Width / 2, canvas.Height);
+            Point p3 = new Point(0,canvas.Height/2 );
+            Point p4 = new Point(canvas.Width , canvas.Height / 2);
+
+            g.DrawLine(myPenRed, p1 ,p2);
+            g.DrawLine(myPenBlue, p3, p4);
+
+            my_angle = my_angle + Int32.Parse(angle.Text);
+            my_lenght = my_lenght + my_increment;
+
+            end_x = (int)(start_x + Math.Cos(my_angle * .017453292519) * my_lenght);
+            end_y = (int)(start_y + Math.Cos(my_angle * .017453292519) * my_lenght);
+
+
+
+            Point[] points =
+            {
+                new Point(start_x,start_y),
+                new Point(end_x,end_y)
+            };
+
+            start_x = end_x;
+            start_y = end_y;
+
+            g.DrawLines(myPen, points);
+
+        }
+
+       
+
+        private void canvas_Paint_1(object sender, PaintEventArgs e)
+        {
+            myPen.Width = 1;
+            my_lenght = Int32.Parse(lenght.Text);
+            g = canvas.CreateGraphics();
+            for (int i = 0; i < Int32.Parse(lines.Text); i++)
+                drawLine();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+
+        private void canvas_Resize(object sender, EventArgs e)
+        {
+            canvas.Refresh();
+        }
+    }
+}
